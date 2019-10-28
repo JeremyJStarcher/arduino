a694a89c94b8a659b590140fa06ef5c77a5d9b28
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-10-27 22:18:40 -0400

    Renamed 'test' to 'runner'
    Since it didn't actually hold any tests.

--- serial-crc/3/test/go.sh
+++ serial-crc/3/test/go.sh
@@ -10,8 +10,8 @@ DEFINES="-DBAUD=B${BAUD} -DSLAVE_DEVICE=${SLAVE_DEVICE} -DMASTER_DEVICE=${MASTER
 
 GCC_ARGS="-g3 -Wall "
 
-g++ ${GCC_ARGS} test.cpp ${DEFINES} -DMASTER -o /tmp/master
-g++ ${GCC_ARGS} test.cpp ${DEFINES} -DSLAVE -o /tmp/slave
+g++ ${GCC_ARGS} runner.cpp ${DEFINES} -DMASTER -o /tmp/master
+g++ ${GCC_ARGS} runner.cpp ${DEFINES} -DSLAVE -o /tmp/slave
 
 xterm -e /tmp/master &
 xterm -e /tmp/slave &
