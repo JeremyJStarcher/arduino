c4bf05cce7fbdf1f3567057f717ac14e206660a3
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-11-10 07:28:42 -0500

    The minicom won't fire if compiled code fails

--- serial-crc/3/build.sh
+++ serial-crc/3/build.sh
@@ -1,5 +1,8 @@
 killall minicom
 
+rm *.elf
+rm *.hex
+
 MASTER_DEVICE=/dev/ttyACM1
 SLAVE_DEVICE=/dev/ttyACM2
 
@@ -13,8 +16,11 @@ arduino-cli upload --port ${SLAVE_DEVICE} --fqbn arduino:avr:mega `pwd` ${UPLOAD
 echo Uploadig to master
 arduino-cli upload --port ${MASTER_DEVICE} --fqbn arduino:avr:mega `pwd` ${UPLOAD_FLAGS}
 
+
+if [ -f 3.arduino.avr.mega.elf ]; then
+  minicom --device ${MASTER_DEVICE} --baud 9600  --capturefile /tmp/master.cap
+  minicom --device ${SLAVE_DEVICE} --baud 9600 --capturefile /tmp/slave.cap
+fi
+
 rm *.elf
 rm *.hex
-
-minicom --device ${MASTER_DEVICE} --baud 9600  --capturefile /tmp/master.cap
-minicom --device ${SLAVE_DEVICE} --baud 9600 --capturefile /tmp/slave.cap
