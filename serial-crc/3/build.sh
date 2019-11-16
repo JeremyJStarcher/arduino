3cb55cbb75c75b4af7ce38dba93234da031b66bd
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-11-16 02:56:04 -0500

    Re-arranged build order to fit USB order

--- serial-crc/3/build.sh
+++ serial-crc/3/build.sh
@@ -10,12 +10,11 @@ UPLOAD_FLAGS="--verify --verbose"
 DEFINES=
 arduino-cli compile --fqbn arduino:avr:mega `pwd`
 
-echo Uploading to slave
-arduino-cli upload --port ${SLAVE_DEVICE} --fqbn arduino:avr:mega `pwd` ${UPLOAD_FLAGS}
-
 echo Uploadig to master
 arduino-cli upload --port ${MASTER_DEVICE} --fqbn arduino:avr:mega `pwd` ${UPLOAD_FLAGS}
 
+echo Uploading to slave
+arduino-cli upload --port ${SLAVE_DEVICE} --fqbn arduino:avr:mega `pwd` ${UPLOAD_FLAGS}
 
 if [ -f 3.arduino.avr.mega.elf ]; then
   minicom --device ${MASTER_DEVICE} --baud 9600  --capturefile /tmp/master.cap
