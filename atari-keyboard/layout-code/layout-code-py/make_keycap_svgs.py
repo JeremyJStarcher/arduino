91666e98adfeb9ad7bd369dfa07f3a2c90ad2579
Jeremy J Starcher <jeremy.starcher@gmail.com>
2021-10-18 01:56:13 -0400

    Added icons for the four suits of cards

--- atari-keyboard/layout-code/layout-code-py/make_keycap_svgs.py
+++ atari-keyboard/layout-code/layout-code-py/make_keycap_svgs.py
@@ -9,7 +9,7 @@ pp = pprint.PrettyPrinter(indent=4)
 cssPath = "./layout.css"
 outPath = './svg'
 bgColor = "transparent"
-fgColor = "black"
+fgColor = "gray"
 
 if not os.path.isdir(outPath):
     print('Creating:', outPath)
@@ -79,9 +79,17 @@ def get_svg_paths() -> Dict[str, str]:
             <rect x="0" y="4" width="4" height="4" style="fill:{fgColor}"  />
             """,
         "ctrl-p": f"""
-            <text name="-1" x="0" y="8"
-            font="Arial" rotate="0" horizAnchor="middle" vertAnchor="middle" 
-            scale="0.25,0.25" width="0.1" fill="{fgColor}" stroke="{fgColor}">&#x2663;</text>
+            <g transform="translate(-1)">
+            <g transform="matrix(0.20000002 0 0 0.20000002 1 -0)">
+            <path d="M20 0C 15.269 0 11.429 4.032 11.429 9C 11.47 12.125999 13.083 14.768 14.762 17.281C 12.891 15.865 
+            10.811 15.009 8.5714 15C 3.8399997 15 0 19.032 0 24C 0 28.968 3.84 33 8.5714 33C 12.403999 32.936 15.469999 
+            30.254 18.481998 28C 17.942999 34.733 16.846998 38.514 10.475999 40L10.475999 40L29.523998 40C 23.152998 
+            38.514 22.057 34.733 21.517998 28C 24.494997 30.552 27.617998 32.717 31.428997 33C 36.159996 33 39.999996 
+            28.968 39.999996 24C 39.999996 19.032 36.159996 15 31.428997 15C 29.131996 15 27.147997 16.057 25.237997 
+            17.281C 27.137997 14.794001 28.388996 12.111 28.570997 9C 28.570997 4.032 24.730997 0 19.999996 0L19.999996
+             0L20 0z" stroke="none" fill="{fgColor}" fill-rule="nonzero" />
+            </g>
+            </g>
             """,
         "ctrl-q": f"""
             {down}
@@ -124,21 +132,42 @@ def get_svg_paths() -> Dict[str, str]:
             {right}
             """,
         "ctrl-comma": f"""
-            <text name="-1" x=".5" y="8"
-            font="Arial" rotate="0" horizAnchor="middle" vertAnchor="middle" 
-            scale="0.25,0.25" width="0.1" fill="{fgColor}" stroke="{fgColor}">&#9829;</text>
-            """,
+            <g transform="translate(-1)">
+            <g transform="matrix(0.011111112 0 0 0.011111112 1 -0)">
+            <path d="M180 45C 80.64 45 0 125.64 0 225C 0 272.8 18.66 316.26 49.094 348.5L49.094 348.5L359.004 
+            675L674.344 345.69C 705.664 307.68 716.194 275.48 720.00397 225C 720.00397 125.64 639.36395 45 540.00397
+             45C 448.45398 45 372.79395 113.48 361.47397 201.97L361.47397 201.97L358.53397 201.97C 347.21396 113.48
+              271.55396 45 180.00397 45L180.00397 45L180 45z" stroke="none" fill="{fgColor}" fill-rule="nonzero" />
+            </g>
+            </g>
+          """,
         "ctrl-period": f"""
-            <text name="-1" x="2" y="6.5"
-            font="Arial" rotate="0" horizAnchor="middle" vertAnchor="middle" 
-            scale="0.05,0.05" width="0.01" font-size="8px" fill="{fgColor}" stroke="{fgColor}">&#9830;</text>
-            """,
+            <g transform="rotate(45)">
+            <rect x="3" y="-2.5" width="5" height="5" style="fill:{fgColor}"  />
+            </g>
+          """,
         "ctrl-semi": f"""
-            <text name="-1" x="2" y="6.5"
-            font="Arial" rotate="0" horizAnchor="middle" vertAnchor="middle" 
-            scale="0.05,0.05" width="0.01" font-size="8px" fill="{fgColor}"
-            stroke="{fgColor} stroke-width:0.01
-            ">&#9824;</text>
+           <g transform="translate(-.5, -.2)">
+            <g transform="matrix(0.09345795 0 0 0.09345795 0 0)">
+            <g transform="matrix(1 0 0 1 -296.25 7.5)">
+                <g transform="matrix(1 0 0 1 62.596 -152.93)">
+                <path d="M283.41 149.59C 272.46 168.20999 230.03 201.59999 255.53 211.72C 271.01 216.38 278.38 206.84
+                281.5 200.53L281.5 200.53L282.12 200.53C 282.1 201.72 281.55 229.91 279.91 231.28C 276.31 234.29
+                271.75 234.06 271.75 234.06L271.75 234.06L271.84 237.34L295.66 237.25L295.75 234C 295.75 234 291.22
+                234.2 287.62 231.19C 285.97998 229.81 285.4 201.67 285.38 200.5L285.38 200.5L285.62 200.5C 288.15 
+                205.66 297.71 216.33 308.22998 212.27C 338.99 200.87001 292.75998 170.3 283.40997 149.59L283.40997 
+                149.59L283.41 149.59z" stroke="{fgColor}" stroke-width="1.25" fill="{fgColor}" fill-rule="nonzero" />
+                </g>
+                <path d="M345.98 -3.1575C 335.03 15.462501 292.6 48.8505 318.1 58.9685C 333.58002 63.6255 340.95 
+                54.0915 344.07 47.7805L344.07 47.7805L344.7 47.7805C 344.67 48.9675 344.12003 77.156494 342.48 78.5305C
+                338.88 81.5445 334.32 81.3115 334.32 81.3115L334.32 81.3115L334.42 84.5935L358.23 84.4995L358.32
+                81.2495C 358.32 81.2495 353.79 81.45049 350.2 78.43649C 348.56 77.06249 347.97 48.920494 347.95
+                47.749493L347.95 47.749493L348.2 47.749493C 350.72 52.906494 360.28 63.582493 370.80002 59.516495C
+                401.56003 48.119495 355.33002 17.551495 345.98 -3.157505L345.98 -3.157505L345.98 -3.1575z"
+                stroke="none" fill="{fgColor}" />
+            </g>
+        </g>
+        </g>
             """,
     }
     return bodies
@@ -160,7 +189,7 @@ def normalize_svg(svg: str) -> str:
 
 
 def wrap_svg_path(s: str) -> str:
-    border_color = "transparent"
+    border_color = fgColor
 
     svg_fill = f"""<rect x="0" y="0" width="8" height="8" 
                style="fill:{bgColor};stroke-width:1;stroke:{border_color}"  />"""
@@ -196,6 +225,7 @@ def html_encode(s: str) -> str:
 
     return s
 
+
 def make_svgs() -> None:
     paths = get_svg_paths()
     for key, path in paths.items():
@@ -208,11 +238,8 @@ def make_svgs() -> None:
 
 def make_css() -> None:
     t = """.at-ascii {
-  display: inline-block;
-  transform: translate(0, 0);
-  border: solid 1px;
-  
-    border: 1px solid red;
+    display: inline-block;
+    transform: translate(0, 0);
     min-height: 20px;
     width: 10px;
 }""".splitlines(False)
@@ -223,7 +250,7 @@ def make_css() -> None:
     for key, path in paths.items():
         full_svg = wrap_svg_path(path)
         normalized = normalize_svg(full_svg)
-        encoded = normalized # html_encode(normalized)
+        encoded = normalized  # html_encode(normalized)
         out.append(f'.at-ascii-{key}:after {{content: url("data:image/svg+xml,{encoded}"); }}')
 
     with open(f"{cssPath}", 'w') as f:
@@ -231,3 +258,4 @@ def make_css() -> None:
 
 
 make_css()
+make_svgs()
