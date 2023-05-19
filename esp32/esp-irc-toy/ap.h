c7dc6a73f08b234c7128b3a778f75e73147995d3
Jeremy J Starcher <jeremy.starcher@gmail.com>
2023-05-18 20:00:32 -0400

    Cleaned up the internal code a lot.

--- esp32/esp-irc-toy/ap.h
+++ esp32/esp-irc-toy/ap.h
@@ -34,7 +34,7 @@ void setupAP() {
 }
 
 
-String getAndSend(int phrase) {
+String htmlEncodeEpromString(int phrase) {
   static char buff[100];
   String output(getEpromString(phrase, buff, 100));
 
@@ -44,6 +44,169 @@ String getAndSend(int phrase) {
   output.replace("<", "&lt;");
   output.replace(">", "&gt;");
   return output;
+}
+
+const char* getSettingsPageHtml() {
+  const char* htmlResponse = R"rawliteral(
+HTTP/1.1 200 OK
+Content-type:text/html
+
+<html>
+<head>
+  <title>ESP32 TOY Setup</title>
+  <meta charset='UTF-8'>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <style>
+    body {
+      background-color: navy;
+      color: yellow;
+    }
+
+    .wide-input label {
+      display: inline-block;
+      width: 15em;
+      background-color: black;
+      color: yellow;
+    }
+    .wide-input input {
+      display: inline-block;
+      width: 15em;
+    }
+  </style>
+</head>
+<body>
+  <h1>ESP32 TOY Setup</h1>
+  <form action='' method='get'>
+    <fieldset>
+      <legend>WIFI Configuration</legend>
+      <div class='wide-input'>
+        <label for='ssid'>SSID (Wifi Name)</label>
+        <input value='%s' id='ssid' name='ssid'>
+      </div>
+      <div class='wide-input'>
+        <label for='pd'>Password</label>
+        <input id='pw' value='%s' type='password' name='pw'>
+      </div>
+      <div>
+        <label for='showPasswordCheckbox'>Show Password</label>
+        <input type='checkbox' id='showPasswordCheckbox' onchange='togglePasswordVisibility()'>
+      </div>
+    </fieldset>
+    <fieldset>
+      <legend>IRC Configuration</legend>
+ 
+      <div class='wide-input'>
+        <label for='irc_server'>Server</label>
+        <input id='irc_server' value='%s' name='irc_server'>
+      </div>
+ 
+      <div class='wide-input'>
+        <label for='irc_port'>Port</label>
+        <input id='irc_port' value='%s' name='irc_port'>
+      </div>
+      <div class='wide-input'>
+        <label for='irc_nickname'>Nickname</label>
+        <input id='irc_nickname' value='%s' name='irc_nickname'>
+      </div>
+      <div class='wide-input'>
+        <label for='irc_user'>User</label>
+        <input id='irc_user' value='%s' name='irc_user'>
+      </div>
+      <div class='wide-input'>
+        <label for='irc_fullname'>Full Name</label>
+        <input id='irc_fullname' value='%s' name='irc_fullname'>
+      </div>
+    </fieldset>
+    <button type='button' onclick='save()'>Save</button>
+  </form>
+  Click <a href='/H'>here</a> to turn ON the LED.<br>
+  Click <a href='/L'>here</a> to turn OFF the LED.<br>
+
+  <script>
+    function g(id) {
+      const encoder = new TextEncoder();
+      return encoder.encode(document.getElementById(id).value);
+    }
+    function save() {
+      const l = [
+        ...g('ssid'), 0,
+        ...g('pw'), 0,
+        ...g('irc_server'), 0,
+        ...g('irc_port'), 0,
+        ...g('irc_nickname'), 0,
+        ...g('irc_user'), 0,
+        ...g('irc_fullname'), 0,
+        -1
+      ];
+      fetch(`save/${l}`);
+    }
+
+    function togglePasswordVisibility() {
+      const passwordInput = document.getElementById('pw');
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+      } else {
+        passwordInput.type = 'password';
+      }
+    }
+  </script>
+</body>
+</html>
+)rawliteral";
+
+  return htmlResponse;
+}
+
+void saveSettings(String currentLine) {
+  std::string::size_type l = currentLine.length();
+
+  std::string::size_type start = 0;
+  std::string::size_type fin = 0;
+
+  // Eat the first slash
+  while (currentLine[start] != '/') {
+    start += 1;
+  }
+  start += 1;
+
+  // Eat the second slash
+  while (currentLine[start] != '/') {
+    start += 1;
+  }
+  start += 1;
+
+  // Read to the space
+  fin = start;
+  while (currentLine[fin] != ' ') {
+    fin += 1;
+  }
+
+  // Force the string to end right there
+  currentLine[fin] = 0;
+
+  int idx = 0;
+  char* ptr = NULL;
+  ptr = strtok(&currentLine[start], ",");
+  while (ptr != NULL) {
+    signed int ch = atoi(ptr);
+    if (ch == -1) {
+      break;
+    }
+    int offset = EEPROM_DATA_OFFSET + idx;
+    EEPROM.write(offset, ch);
+    ptr = strtok(NULL, ",");
+    Serial.print("SSID Saving ");
+    Serial.print((int)ch);
+    Serial.print(" ");
+    Serial.print((char)ch);
+    Serial.print(" at ");
+    Serial.println(offset);
+
+    idx += 1;
+  }
+  EEPROM.commit();
+
+  Serial.println("******");
 }
 
 void loopAP() {
@@ -61,125 +224,25 @@ void loopAP() {
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
 
-            client.print("<html><head><title>ESP32 TOY Setup</title>");
-            client.print("<style> label {display: inline-block: width: 25em; background-color: black; color: yellow;}</style>");
+            client.printf(
+              getSettingsPageHtml(),
+              htmlEncodeEpromString(EEPROM_SSID_NAME).c_str(),
+              htmlEncodeEpromString(EEPROM_SSID_PW).c_str(),
 
-            client.print("</head>");
-            client.print("<body><h1>ESP32 TOY Setup</h1>");
+              htmlEncodeEpromString(EEPROM_IRC_SERVER).c_str(),
+              htmlEncodeEpromString(EEPROM_IRC_PORT).c_str(),
+              htmlEncodeEpromString(EEPROM_IRC_NICK).c_str(),
+              htmlEncodeEpromString(EEPROM_IRC_USER).c_str(),
+              htmlEncodeEpromString(EEPROM_IRC_FULLNAME).c_str());
 
-            client.print("<form action='' method='get'>");
-            client.print("<fieldset>");
-            client.print("<legend>WIFI Configuration</legend>");
-            client.printf("<div><label>SSID (Wifi Name)</label><input value='%s' id='ssid' name='ssid'></div>", getAndSend(EEPROM_SSID_NAME).c_str());
-            client.printf("<div><label>Password</label><input id='pw' value='%s' type='password' name='pw'></div>", getAndSend(EEPROM_SSID_PW).c_str());
-            client.print("</fieldset>");
-
-            client.print("<fieldset>");
-            client.print("<legend>IRC Configuration</legend>");
-            client.printf("<div><label>Server</label><input id='irc_server' value='%s' name='irc_server'></div>", getAndSend(EEPROM_IRC_SERVER).c_str());
-            client.printf("<div><label>Port</label><input id='irc_port' value='%s' name='irc_port'></div>", getAndSend(EEPROM_IRC_PORT).c_str());
-            client.printf("<div><label>Nickname</label><input id='irc_nickname' value='%s' name='irc_nickname'></div>", getAndSend(EEPROM_IRC_NICK).c_str());
-            client.printf("<div><label>User</label><input id='irc_user' value='%s' name='irc_user'></div>", getAndSend(EEPROM_IRC_USER).c_str());
-            client.printf("<div><label>Full Name</label><input id='irc_fullname' value='%s' name='irc_fullname'></div>", getAndSend(EEPROM_IRC_FULLNAME).c_str());
-            client.print("</fieldset>");
-
-            client.print("<button type='button' onclick='save()'>Save</button></form>");
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            client.println("<script>");
-            client.println("function g(id) {");
-            client.println("  const encoder = new TextEncoder();");
-            client.println("  return encoder.encode(document.getElementById(id).value);");
-            client.println("}");
-            client.println("function save() {");
-            client.println("  const l = [");
-            client.println("    ...g('ssid'), 0,");
-            client.println("    ...g('pw'), 0,");
-            client.println("    ...g('irc_server'), 0,");
-            client.println("    ...g('irc_port'), 0,");
-            client.println("    ...g('irc_nickname'), 0,");
-            client.println("    ...g('irc_user'), 0,");
-            client.println("    ...g('irc_fullname'), 0,");
-            client.println("    -1");
-            client.println("  ];");
-
-            client.println("  debugger;");
-            client.println("  fetch(`save/${l}`);");
-
-            client.println("}");
-
-            client.println("</script>");
-
-            client.print("</body>");
-            client.print("</html>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
             // break out of the while loop:
             break;
           } else {  // if you got a newline, then clear currentLine:
             Serial.println("Current line: ");
             Serial.println(currentLine);
             if (currentLine.startsWith("GET /save/")) {
-              std::string::size_type l = currentLine.length();
-
-              std::string::size_type start = 0;
-              std::string::size_type fin = 0;
-
-              // Eat the first slash
-              while (currentLine[start] != '/') {
-                start += 1;
-              }
-              start += 1;
-
-              // Eat the second slash
-              while (currentLine[start] != '/') {
-                start += 1;
-              }
-              start += 1;
-
-              // Read to the space
-              fin = start;
-              while (currentLine[fin] != ' ') {
-                fin += 1;
-              }
-
-              // Force the string to end right there
-              currentLine[fin] = 0;
-
-
-              int idx = 0;
-              char *ptr = NULL;
-              ptr = strtok(&currentLine[start], ",");
-              while (ptr != NULL) {
-                signed int ch = atoi(ptr);
-                if (ch == -1) {
-                  break;
-                }
-                int offset = EEPROM_DATA_OFFSET + idx;
-                EEPROM.write(offset, ch);
-                ptr = strtok(NULL, ",");
-                Serial.print("SSID Saving ");
-                Serial.print((int)ch);
-                Serial.print(" ");
-                Serial.print((char)ch);
-                Serial.print(" at ");
-                Serial.println(offset);
-
-                idx += 1;
-              }
-              EEPROM.commit();
-
-              Serial.println("******");
+              saveSettings(currentLine);
             }
 
             char ssid_buf[SSID_MAX_LEN];
@@ -191,9 +254,6 @@ void loopAP() {
             Serial.println("-=-=-=-=-=-=-=-=-=-=-=-");
             Serial.println(ssid_buf);
             Serial.println(pw_buf);
-
-            ///Current line: GET /save/113,113,113,0,122,122,122,0 HTTP/1.1
-
 
             currentLine = "";
           }
